cc535e03e1ee6de26b3d3e7b65bb9e35f6211901
Dariusz Smigiel <dsmigiel@redhat.com>
2026-03-13 19:04:27 -0700

    wcag 2.1 AA fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://sc4wdc.com',
   base: '/',
   output: 'static',
+  integrations: [sitemap()],
   build: {
     assets: '_assets'
   }
